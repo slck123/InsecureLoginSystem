12d43352975fa958c7e4f656787a4bc73e7c1c20
selcu <selcu@SELCUK>
2025-01-18 19:20:22 +0300

    Proje dosyası ekle.

---
+++ InsecureLoginSystem/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        txtUsername = New TextBox()
+        txtPassword = New TextBox()
+        btnLogin = New Button()
+        lblResult = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(24, 34)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(88, 20)
+        Label1.TabIndex = 0
+        Label1.Text = "kullanıcı adı"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(24, 87)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(37, 20)
+        Label2.TabIndex = 1
+        Label2.Text = "şifre"
+        ' 
+        ' txtUsername
+        ' 
+        txtUsername.Location = New Point(163, 31)
+        txtUsername.Name = "txtUsername"
+        txtUsername.Size = New Size(125, 27)
+        txtUsername.TabIndex = 2
+        ' 
+        ' txtPassword
+        ' 
+        txtPassword.Location = New Point(163, 80)
+        txtPassword.Name = "txtPassword"
+        txtPassword.Size = New Size(125, 27)
+        txtPassword.TabIndex = 3
+        ' 
+        ' btnLogin
+        ' 
+        btnLogin.Location = New Point(163, 197)
+        btnLogin.Name = "btnLogin"
+        btnLogin.Size = New Size(94, 29)
+        btnLogin.TabIndex = 4
+        btnLogin.Text = "giriş"
+        btnLogin.UseVisualStyleBackColor = True
+        ' 
+        ' lblResult
+        ' 
+        lblResult.AutoSize = True
+        lblResult.Location = New Point(483, 38)
+        lblResult.Name = "lblResult"
+        lblResult.Size = New Size(0, 20)
+        lblResult.TabIndex = 5
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblResult)
+        Controls.Add(btnLogin)
+        Controls.Add(txtPassword)
+        Controls.Add(txtUsername)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtUsername As TextBox
+    Friend WithEvents txtPassword As TextBox
+    Friend WithEvents btnLogin As Button
+    Friend WithEvents lblResult As Label
+
+End Class
---
+++ InsecureLoginSystem/Form1.vb
@@ -0,0 +1,72 @@
+﻿
+Imports System.Data.OleDb
+
+
+
+
+
+Public Class Form1
+
+
+    Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=your source;"
+    Dim connection As New OleDbConnection(connectionString)
+
+
+
+
+
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        Dim username As String = txtUsername.Text
+        Dim password As String = txtPassword.Text
+
+        ' Veritabanı bağlantısı
+        Dim connectionString As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=your source;"
+        Dim query As String = $"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
+
+        Try
+            Using connection As New OleDbConnection(connectionString)
+                Dim command As New OleDbCommand(query, connection)
+                connection.Open()
+                Dim reader As OleDbDataReader = command.ExecuteReader()
+
+                If reader.HasRows Then
+                    lblResult.Text = "Giriş başarılı!"
+                Else
+                    lblResult.Text = "Giriş başarısız!"
+                End If
+            End Using
+        Catch ex As Exception
+            lblResult.Text = "Hata: " & ex.Message
+        End Try
+
+
+        Try
+            Using connection As New OleDbConnection(connectionString)
+                Dim command As New OleDbCommand(query, connection)
+                connection.Open()
+                Dim reader As OleDbDataReader = command.ExecuteReader()
+
+                If reader.HasRows Then
+                    lblResult.Text = "Giriş başarılı!"
+                Else
+                    lblResult.Text = "Giriş başarısız!"
+                End If
+            End Using
+        Catch ex As Exception
+            lblResult.Text = "Hata: " & ex.Message & " - Query: " & query
+        End Try
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+End Class
